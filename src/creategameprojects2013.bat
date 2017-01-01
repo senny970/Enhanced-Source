b1bb9ea294460b33eae9c784f357bc0298f8bbe0
Joshua Ashton <joshua@corruptedstudios.net>
2017-01-01 23:03:50 +0000

    Fix naming conventions

--- src/creategameprojects2013.bat
+++ src/creategameprojects2013.bat
@@ -1,3 +1,3 @@
 devtools\bin\vpc.exe /template +game /mksln Game_Template-2013.sln /2013
 devtools\bin\vpc.exe /portal +game /mksln Game_Portal-2013.sln /2013
-devtools\bin\vpc.exe /hl2 +game /mksln Game_HL2-2010.sln /2013
+devtools\bin\vpc.exe /hl2 +game /mksln Game_HL2-2013.sln /2013
